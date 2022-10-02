11c9f86745cb1d45c72ac1c1310785a0ac551efd
GodPlaysDice <49920714+GodDoesPlayDice@users.noreply.github.com>
2022-10-02 16:17:22 +0400

    fixed enemy attack rate

--- Assets/Scripts/AI/States/ChaseState.cs
+++ Assets/Scripts/AI/States/ChaseState.cs
@@ -1,3 +1,5 @@
+using System;
+using UnityEngine;
 using UnityEngine.AI;
 
 namespace AI.States
@@ -6,19 +8,37 @@ namespace AI.States
     {
         // for calculations 
         private NavMeshPath _navMeshPath;
+        private Vector3 _targetPos;
 
         public override State RunCurrentState()
         {
+            Agent.speed = Controller.ChaseSpeed;
+            _targetPos = Controller.TargetPosition +
+                         (transform.position - Controller.TargetPosition).normalized *
+                         (Controller.AttackDistance - .5f);
+
+            Agent.SetDestination(_targetPos);
+            // rotation towards player 
+            // var targetRotation =
+            //     Quaternion.LookRotation((Controller.TargetPosition - Agent.transform.position).normalized);
+            // Agent.transform.rotation =
+            //     Quaternion.Slerp(Agent.transform.rotation, targetRotation, 5f * Time.deltaTime);
+
+
             if (Controller.ShouldAttackTarget)
                 return StateManager.AttackState;
             if (!Controller.ShouldChaseTarget)
                 return StateManager.WanderState;
 
-            Agent.speed = Controller.ChaseSpeed;
-            var targetPos = Controller.TargetPosition +
-                            (transform.position - Controller.TargetPosition).normalized; 
-            Agent.SetDestination(targetPos);
             return this;
         }
+
+#if UNITY_EDITOR
+        private void OnDrawGizmos()
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawSphere(_targetPos, 0.2f);
+        }
+#endif
     }
 }
--- Assets/Scripts/EnemyAnimations.cs
+++ Assets/Scripts/EnemyAnimations.cs
@@ -1,19 +1,23 @@
 using System;
 using AI;
+using AI.States;
 using UnityEngine;
 
 public class EnemyAnimations : CharacterAnimations
 {
     private StateManager _stateManager;
+    private AttackState _attackState;
     private float _targetSpeed;
 
     private static readonly int LightAttack = Animator.StringToHash("LightAttack");
+    private static readonly int HeavyAttack = Animator.StringToHash("HeavyAttack");
     private static readonly int Speed = Animator.StringToHash("Speed");
 
     protected override void Awake()
     {
         base.Awake();
         _stateManager = GetComponentInChildren<StateManager>();
+        _attackState = _stateManager.GetComponentInChildren<AttackState>();
         _stateManager.OnStateChange += OnStateChange;
     }
 
@@ -26,7 +30,7 @@ public class EnemyAnimations : CharacterAnimations
     private void OnStateChange(State newState)
     {
         if (newState == _stateManager.AttackState)
-            Animator.SetTrigger(LightAttack);
+            Animator.SetTrigger(_attackState.CurrentAttackType == AttackType.Light ? LightAttack : HeavyAttack);
         if (newState == _stateManager.ChaseState)
             _targetSpeed = 6f;
         if (newState == _stateManager.WanderState)
