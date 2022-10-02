d8ad08d3705fb271c48d06023e5d75f59b97a2ca
GastarTestProjects <53343040+GastarTestProjects@users.noreply.github.com>
2022-10-02 19:51:06 +0400

    Add upgrade data

--- Assets/Data/Weapons/WeaponData.cs
+++ Assets/Data/Weapons/WeaponData.cs
@@ -10,7 +10,9 @@ public class WeaponData : ScriptableObject
     public bool purchased;
     public int currentLvl = 0;
     public int cost;
+    public int upgradeCost;
     public float damage;
+    public float upgradeDamage;
     public float coolDown;
     public float distance = 10;
     public float aoeArea;
