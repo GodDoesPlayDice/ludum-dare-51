e3b9177c83cb9fe45e6b68292d8f686c22e2c72b
GodPlaysDice <49920714+GodDoesPlayDice@users.noreply.github.com>
2022-10-19 17:02:51 +0400

    deleted timer from HUD and working on BoonsDisplay part

--- Assets/Scripts/UI/HudController.cs
+++ Assets/Scripts/UI/HudController.cs
@@ -12,27 +12,17 @@ namespace UI
         [SerializeField] private Slider staminaSlider;
         [SerializeField] private Slider healthSlider;
         [SerializeField] private TextMeshProUGUI healthText;
-        [SerializeField] private Button showUpgradesButton;
-        [SerializeField] private Slider timeSlider;
-        [SerializeField] private GameObject upgradeAvailableText;
 
         private Character _character;
         private Stamina _stamina;
 
-        private UpgradeManager _upgradeManager;
-        private UpgradeMenuController _upgradePanel;
-        private Timer _timer;
-
-        private Tweener _tweener;
+        private UpgradeMenuController _upgradeMenu;
 
         private void Awake()
         {
             _character = GetComponentInParent<Character>();
             _stamina = _character.GetComponent<Stamina>();
-
-            _upgradeManager = _character.GetComponentInChildren<UpgradeManager>();
-            _upgradePanel = _character.GetComponentInChildren<UpgradeMenuController>();
-            _timer = _character.GetComponentInChildren<Timer>();
+            _upgradeMenu = _character.GetComponentInChildren<UpgradeMenuController>();
 
             UpdateHealth(_character.Health);
             UpdateMaxHealth(_character.MaxHealth);
@@ -43,14 +33,11 @@ namespace UI
             _character.OnMaxHealthChange += UpdateMaxHealth;
             _stamina.OnCurrentStaminaChange += UpdateStamina;
             _stamina.OnMaxStaminaChange += UpdateMaxStamina;
-
-            _upgradeManager.OnAvailableLvlChange += UpdateUpgradeButtonLevel;
-            _timer.OnCurrentCycleTimeChange += UpdateTimer;
         }
 
         public void ShowUpgradePanel()
         {
-            _upgradePanel.Show();
+            _upgradeMenu.Show();
         }
 
         private void UpdateHealth(float health)
@@ -78,25 +65,6 @@ namespace UI
         {
             if (maxStamina != 0)
                 staminaSlider.value = _stamina.CurrentStamina / maxStamina;
-        }
-
-        private void UpdateUpgradeButtonLevel(int lvlAvailable)
-        {
-            showUpgradesButton.GetComponentInChildren<TMP_Text>().text = lvlAvailable.ToString();
-            var available = lvlAvailable > 0;
-            showUpgradesButton.interactable = available;
-            upgradeAvailableText.gameObject.SetActive(available);
-            if (available && _tweener is not {active: true})
-            {
-                upgradeAvailableText.transform.rotation = Quaternion.identity;
-                _tweener = upgradeAvailableText.transform.DOPunchRotation(new Vector3(0f, 0f, 10f), .5f)
-                    .SetLoops(10000);
-            }
-        }
-
-        private void UpdateTimer(float time)
-        {
-            timeSlider.value = time;
         }
     }
 }
