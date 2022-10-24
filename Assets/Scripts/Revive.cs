c6c60c012b356e6adfac1ab98c5467acb6d89517
GastarTestProjects <53343040+GastarTestProjects@users.noreply.github.com>
2022-10-24 15:28:32 +0400

    Spawn effect tweaks

--- Assets/Scripts/Revive.cs
+++ Assets/Scripts/Revive.cs
@@ -1,13 +1,17 @@
+using DG.Tweening;
 using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.TextCore.Text;
+using static UnityEngine.ParticleSystem;
 
 public class Revive : MonoBehaviour
 {
     [SerializeField][Range(0f, 10f)] float reviveEffectsDelay = 2f;
+    [SerializeField][Range(0f, 5f)] float spawnDuration = 0.7f;
     [SerializeField] GameObject reviveEffect;
+    [SerializeField] GameObject spawnEffect;
     [SerializeField] GameObject mainModel;
 
     private Enemy _enemy;
@@ -19,18 +23,33 @@ public class Revive : MonoBehaviour
         _enemy.OnStartSpawn += callback =>
         {
             StopAllCoroutines();
-            StartCoroutine(ReviveEffects(reviveEffectsDelay, callback));
+            StartCoroutine(ReviveEffects(callback));
         };
     }
 
-    public IEnumerator ReviveEffects(float delay, Action callback)
+    public IEnumerator ReviveEffects(Action callback)
     {
         //_enemy.enabled = true;
         mainModel.SetActive(false);
         reviveEffect.SetActive(true);
-        yield return new WaitForSeconds(delay);
+        yield return new WaitForSeconds(reviveEffectsDelay);
+        StartSpawn(callback);
+    }
+
+    public void StartSpawn(Action callback)
+    {
         mainModel.SetActive(true);
-        reviveEffect.SetActive(false);
-        callback.Invoke();
+        var ySize = gameObject.GetComponentInParent<Collider>().bounds.size.y;
+        //spawnEffect?.SetActive(true);
+        //mainModel.transform.localPosition = mainModel.transform.localPosition + new Vector3(0f, -ySize, 0f);
+        Debug.Log("Start--- " + (mainModel.transform.localPosition + new Vector3(0f, -ySize, 0f)));
+        Debug.Log("Start " + mainModel.transform.localPosition);
+        mainModel.transform.DOLocalMoveY(-ySize, spawnDuration).SetEase(Ease.OutQuart).From().OnComplete(() =>
+        {
+            Debug.Log("End " + mainModel.transform.localPosition);
+            reviveEffect.SetActive(false); // TODO: fade out
+            //spawnEffect?.SetActive(false);
+            callback.Invoke();
+        });
     }
 }
